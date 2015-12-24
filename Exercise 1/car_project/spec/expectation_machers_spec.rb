7be162b689f149e115fd88b59bc9414a0eebf9af
Winbobob <zhu6@ncsu.edu>
2015-12-23 22:12:39 -0500

    Add truthiness matchers code

--- Exercise 1/car_project/spec/expectation_machers_spec.rb
+++ Exercise 1/car_project/spec/expectation_machers_spec.rb
@@ -35,4 +35,31 @@ describe 'Expectation Matchers' do
 			expect(b).to be(c) #synonym for #equal
 		end
 	end
-end
+
+	describe 'truthiness matchers' do
+		it 'will match true/false' do
+			expect(1 < 2).to be(true)
+			expect(1 > 2).to be(false)
+
+			expect('foo').not_to be(true) #the string is not exactly true
+			expect(nil).not_to be(false) #nil is not exactly false
+			expect(0).not_to be(false) #0 is not exactly false
+		end
+
+		it 'will match truthy/falsey' do
+			expect(1 < 2).to be_truthy
+			expect(1 > 2).to be_falsey
+
+			expect('foo').to be_truthy #any value counts as true
+			expect(nil).to be_falsey #nil counts as false
+			expect(0).not_to be_falsey #but 0 is still not falsey enough
+		end
+
+		it 'will match nil' do
+			expect(nil).to be_nil
+			expect(nil).to be(nil) #either way works. I personally prefer second one, since it follows previous syntax.
+			expect(false).not_to be(nil) #just like nil?
+			expect(0).not_to be(nil)
+		end
+	end
+end 
